a8d5b80441167938e58cfb01cd712603bbb6cf2f
Purvi singh <purvisingh2003@gmail.com>
2020-06-10 17:26:38 +0530

    two python files named number task1.py and string task1.py files are added

--- Python-Task1/string task1.py
+++ Python-Task1/string task1.py
@@ -1,13 +1,16 @@
+
 #String concatenation using + operator
 fname=input()
 lname=input()
 name=fname  + lname
 print(name)
+
 #String concatenation using join method
 colgname=input()
 address=input()
 info=(" ".join(colgname,address))
 print(info)
+
 # marks of 5 different subjects
 sub1=float(input())
 print(sub1)
@@ -19,9 +22,11 @@ sub4=float(input())
 print(sub4)
 sub5=float(input())
 print(sub5)
+
 #Calculate total marks
 total_marks=sub1+sub2+sub3+sub4+sub5
 print(total_marks)
+
 #Calculate percentage
 per_marks=((total_marks)/5)*100
 print(total_marks)
