0b91b52b73fc85b4036093552a9dc4f42b02c158
Purvi singh <purvisingh2003@gmail.com>
2020-06-10 17:22:07 +0530

    two python files named number task1.py and string task1.py files are added

---
+++ Python-Task1/number task1.py
@@ -0,0 +1,56 @@
+""" Here num1 and num2 are two integer variables that take only integer values. """
+#Number addition
+num1=int(input())
+num2=int(input())
+print("type of num1",type(num1))
+print("type of num2",type(num2))#type opr returns the type of num2 that it belongs to which class
+add=num1+num2#add is a final variable that store the addition of num1 and num2
+print(add)#print statement displays the final result i.e stored in add
+#Number Subtraction
+sub=num1-num2
+print(sub)
+#Number multiplication
+multiply=num1*num2
+print(multiply)
+#Number Modulus
+mod=num1%num2
+print(mod)# it return the remainder part after division
+#Number exponentiation
+exp=num1**num2
+print(exp)
+# float Number division
+div=num1/num2
+print(div)
+#floor number division
+div1=num1//num2
+print(div1)
+
+# float Number division
+fnum1=float(input())
+fnum2=float(input())
+print("type of fnum1",type(fnum1))
+print("type of fnum2",type(fnum2))
+divide=fnum1/fnum2#divide operator completely divide a number and give ans upto 2 decimal places
+print(divide)
+# floor number division
+floordiv=fnum1//fnum2
+print(floordiv)
+# float addition
+fadd=fnum1+fnum2
+print(fadd)
+#float subtraction
+fsub=fnum1-fnum2
+print(fsub)
+#float multiplication
+fmultiply1=fnum1*fnum2
+print(fmultiply)
+# float Number exponentiation
+fexp=fnum1**fnum2
+print(fexp)
+# float Number Modulus
+fmod=num1%num2
+print(fmod)
+
+
+
+
